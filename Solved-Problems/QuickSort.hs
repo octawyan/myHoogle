e1ce01ad0e6fca394b5807438aa869955c41ff17
Octavian Georgescu <florin.octavian.georgescu@gmail.com>
2015-06-25 19:36:08 +0300

    quicksort :: Ord a => [a] -> [a]

---
+++ Solved-Problems/QuickSort.hs
@@ -0,0 +1,14 @@
+quicksort [] = []
+quicksort l@[_] = l
+quicksort (h:t) =	let
+	lower [] = []
+	lower (h':t') = if (h' < h)	then h' : lower t'
+								else lower t'
+
+	strictlyGreater [] = []
+	strictlyGreater (h'':t'') = if (h'' > h)	then h'' : strictlyGreater t''
+												else strictlyGreater t''
+
+					in quicksort (lower t) ++ [h] ++ quicksort (strictlyGreater t)
+
+	-- quicksort [6,3,1,5,4,2] returns [1,2,3,4,5,6] --
