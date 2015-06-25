d46123cad0fe2406213c4aea5ad0bf531fdcb527
Octavian Georgescu <florin.octavian.georgescu@gmail.com>
2015-06-25 19:56:00 +0300

    mergesort :: Ord a => [a] -> [a]

---
+++ Solved-Problems/MergeSort.hs
@@ -0,0 +1,11 @@
+mergesort [] = []
+mergesort l@[_] = l
+mergesort l = let
+	merge [] l = l
+	merge l [] = l
+	merge l@(h:t) l'@(h':t') = if (h < h')	then h : merge t l'
+	                                        else h' : merge l t'
+	(left, right) = splitAt ((length l) `div` 2) l
+				      in merge (mergesort left) (mergesort right)
+
+-- mergesort [6,3,1,5,4,2] returns [1,2,3,4,5,6] --
