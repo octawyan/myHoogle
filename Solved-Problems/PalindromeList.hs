e9f4b438cc98796ac4cd353af8a3ca9a03bec9a4
Octavian Georgescu <florin.octavian.georgescu@gmail.com>
2015-06-25 19:57:20 +0300

    Delete PalindromeList.hs

--- Solved-Problems/PalindromeList.hs
+++
@@ -1,8 +0,0 @@
-pal l = l == reverse l
--- pal [1,2,3,4,5] returns False --
-
-
-pal' [] = True
-pal' l = if (head l /= last l)	then False
-								else pal' (take (length l - 2) (drop 1 l))
--- pal' [1,3,4,2,4,3,1] returns True --
