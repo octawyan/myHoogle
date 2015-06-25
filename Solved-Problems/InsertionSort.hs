f8729e33b79d6d49c547ffae0a3e20df03968757
Octavian Georgescu <florin.octavian.georgescu@gmail.com>
2015-06-25 19:54:29 +0300

    insertionsort :: Ord a => [a] -> [a]

---
+++ Solved-Problems/InsertionSort.hs
@@ -0,0 +1,9 @@
+insertionsort [] = []
+insertionsort l@[_] = l
+insertionsort (h:t) = let
+	insert x [] = [x]
+	insert x l'@(h':t') = if (x <= h')  then x :l'
+	                                    else h' : insert x t'
+	                    in insert h (insertionsort t)
+
+-- insertionsort [6,3,1,5,4,2] returns [1,2,3,4,5,6] --
